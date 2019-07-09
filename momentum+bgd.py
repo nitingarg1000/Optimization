4a7a6c9b72938ec412454a44a938c3fb5327cb8b
nitingarg1000 <44620248+nitingarg1000@users.noreply.github.com>
2019-07-09 19:42:55 +0530

    Implement momentum with batch gradient descent

---
+++ momentum+bgd.py
@@ -0,0 +1,68 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+dataset = pd.read_csv("dataset.csv", index_col = 0)
+x = np.array(dataset['x'])
+y = np.array(dataset['y'])
+
+def cost(m, t0, t1, x, y):
+	return 1/(2*m) * sum([(t0 + t1* np.asarray([x[i]]) - y[i])**2 for i in range(m)])
+
+
+def gradient_descent(gamma, learning_rate, x, y, iterations):
+	#initialize theta
+	t0 = 0
+	t1 = 0
+
+	#number of examples
+	m = x.shape[0]
+
+	#total error
+	J = cost(m, t0, t1, x, y)
+	velocity0 = np.zeros((iterations))
+	velocity1 = np.zeros((iterations))
+	loss = np.empty(iterations)
+	count = [i for i in range(1, iterations+1)]
+
+	for it in range(iterations):
+		#Calculating the gradients
+		grad0 = 1/m * sum([(t0 + t1*np.asarray([x[i]]) - y[i]) for i in range(m)]) 
+		grad1 = 1/m * sum([(t0 + t1*np.asarray([x[i]]) - y[i])*np.asarray([x[i]]) for i in range(m)])
+		velocity0[it] = gamma*velocity0[it] + learning_rate*grad0
+		velocity1[it] = gamma*velocity1[it] + learning_rate*grad1
+		#Updating the parameters
+		t0 = t0 - velocity0[it]
+		t1 = t1 - velocity1[it]
+		loss[it] = cost(m, t0, t1, x, y)
+
+	return count, loss, t0, t1
+
+
+#Implementation
+
+gamma = 0.9
+alpha = 0.01
+max_iter = 10000
+
+count, loss, theta0, theta1 = gradient_descent(gamma, alpha, x, y, max_iter)
+
+print('theta0 = ' + str(theta0))
+print('theta1 = ' + str(theta1))
+
+plt.figure(0)
+plt.scatter(x, y, c = 'red')
+plt.plot(x, theta0 + theta1 * x)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Dataset')
+plt.savefig('output.png')
+plt.show()
+
+plt.figure(1)
+plt.plot(count, loss)
+plt.xlabel('iteration')
+plt.ylabel('Loss')
+plt.title('Loss History')
+plt.savefig('loss.png')
+plt.show()
